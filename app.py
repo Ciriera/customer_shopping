2f90d660f07b8d8d79e6aa21a155f1fbc39f86e9
Eren CAVULDAK <157847868+Ciriera@users.noreply.github.com>
2025-01-04 18:37:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,6 @@ import time
 import psutil
 from sklearn.cluster import MiniBatchKMeans
 from hdbscan import HDBSCAN
-import fastcluster
 from scipy.cluster.hierarchy import linkage, fcluster
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.metrics import silhouette_score, davies_bouldin_score, calinski_harabasz_score
